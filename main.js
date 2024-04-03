160bfd2596ea318eb1e99c3d63ec71a77a1e7e44
brunacgt <117641824+brunacgt@users.noreply.github.com>
2024-04-03 08:44:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
+
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
 
@@ -12,10 +13,26 @@ for(let i=0;i <botoes.length;i++){
         textos[i].classList.add("ativo");
     }
 }
+
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2029-12-10T00:00:00");
 const tempoObjetivo2 = new Date("2026-04-16T00:00:00");
 
-let tempoAtual= new Date();
+const tempos = [tempoObjetivo1,tempoObjetivo2]
+    
+contadores[0].textContent = calculaTempo(tempos[0]);
 
-contadores[0].textContent = tempoObjetivo1-tempoAtual;
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+ 
+ return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+}

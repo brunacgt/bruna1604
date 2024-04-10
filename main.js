14aad42f92b7191866f10fb863f4f1776da6cba3
brunacgt <117641824+brunacgt@users.noreply.github.com>
2024-04-10 08:45:47 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
 for(let i=0;i <botoes.length;i++){
+    contadores[0].textContent = calculaTempo(tempos[i]);
     botoes[i].onclick = function(){
 
         for(let j=0;j<botoes.length;j++){
@@ -19,8 +20,6 @@ const tempoObjetivo1 = new Date("2029-12-10T00:00:00");
 const tempoObjetivo2 = new Date("2026-04-16T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2]
-    
-contadores[0].textContent = calculaTempo(tempos[0]);
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -36,3 +35,13 @@ function calculaTempo(tempoObjetivo) {
  
  return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 }
+function atualizaCronometro(){
+    for(let i=0; i<contadores.lenght; i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro1000);
+}
+comecaCronometro();
